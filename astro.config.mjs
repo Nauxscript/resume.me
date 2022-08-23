8fa9a45c9dd659aeade1f6adf8160eb66c023392
nauxscript <nauxscript@gmail.com>
2022-08-23 12:16:55 +0800

    :sparkles: use icon in easy way - power by iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config';
 import uno from 'astro-uno'
 // import {  } from 'unocss'
 import presetWind from '@unocss/preset-wind'
+import presetIcons from '@unocss/preset-icons'
 import transformerDirectives from '@unocss/transformer-directives'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     uno({
-      presets: [presetWind()],
+      presets: [presetWind(), presetIcons({
+        prefix: 'i-',
+        extraProperties: {
+          display: 'inline-block'
+        }
+      })],
       shortcuts: [
         {
           'resume-content': 'w-1024px min-h-1440px',
